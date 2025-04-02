bf8aeea96af651fe60f5e911893bb8dfd69cac18
annamariemickova <mickova2@spsejecna.cz>
2025-04-02 18:49:22 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+//import PeriodicTable from "./PeriodicTable";
+
+function App() {
+  return (
+
+      <header>
+        <h1>Periodic Table Explorer</h1>
+      </header>
+
+  )
+}
+
+export default App
